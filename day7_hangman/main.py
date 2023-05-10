a07f672d01b60d88241ed8ee8f38bb405acc2b48
Alex Bang <alexbang@snowshoe.lan>
2023-05-10 08:47:59 -0400

    consolidate import statement

--- day7_hangman/main.py
+++ day7_hangman/main.py
@@ -5,7 +5,7 @@ import random
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 from hangman_words import word_list
-from hangman_art import stages
+from hangman_art import logo, stages
 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -13,8 +13,6 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-from hangman_art import logo
 print(logo)
 
 #Testing code
