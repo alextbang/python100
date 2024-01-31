5c0f4568a55c75d117e04fa142f7acb238aaa053
Alex <bangat@gmail.com>
2024-01-31 09:55:01 -0500

    test

--- day1-10/day1-2.py
+++ day1-10/day1-2.py
@@ -1,11 +1,11 @@
-# Day 01 Band name generator
+# # Day 01 Band name generator
 # print("Hi, welcome to the band name generator app. \n")
 # city = input("What city did you grow up in? \n")
 # pet = input("What is your pet's name? \n")
 # band = city + pet
 # print("Your band name is " + band + ".")
 
-# Day 02 Tip calculator
+# #Day 02 Tip calculator
 # print("Welcome to the tip calculator.")
 # bill = float(input("What was the toal bill? $"))
 # tip = int(input("What percentage tip wold you like to give? 10, 12, or 15? "))
