5735f67e95a1f5e6f432acdbc2847ba11291515d
Alex <bangat@gmail.com>
2024-04-19 12:06:35 -0400

    start functions

---
+++ day1-10/day8.py
@@ -0,0 +1,14 @@
+# define a function
+# def greet():
+#     print("Hello Alex.")
+#     print("Nice to see you!")
+
+# greet()
+
+# define a function with arguments
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"What is lit like in {location}?")
+
+greet_with("Bob", "Maryland")
+ 
